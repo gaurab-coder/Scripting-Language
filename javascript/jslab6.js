2f9a379a52c1ffafaa5f1baf7ff5518898e4a968
gaurab khadka <khadkagaurab82@gmail.com>
2026-08-13 07:43:59 +0545

    break and continue

---
+++ javascript/jslab6.js
@@ -0,0 +1,19 @@
+// Example of break statement
+console.log("Break Example:");
+
+for (let i = 1; i <= 10; i++) {
+    if (i == 5) {
+        break;
+    }
+    console.log(i);
+}
+
+// Example of continue statement
+console.log("Continue Example:");
+
+for (let i = 1; i <= 5; i++) {
+    if (i == 3) {
+        continue;
+    }
+    console.log(i);
+}

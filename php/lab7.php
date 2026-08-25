3f39a60faf0453da459899304f312723e913cb20
gaurab khadka <khadkagaurab82@gmail.com>
2026-08-25 21:04:34 +0545

    date

---
+++ php/lab7.php
@@ -0,0 +1,11 @@
+<?php
+  echo "Today: " . date("Y-m-d") . "<br>";
+  echo "Time: " . date("h:i A") . "<br>";
+  echo "Day: " . date("l");
+?>
+<!-- header.php -->
+<?php echo "<h3>Site Header</h3>"; ?>
+
+<!-- main.php -->
+<?php include("header.php"); ?>
+<p>Main page content.</p>
